e3d742e74d98584b4c1495a2772398b35bba5e69
crochetcode <fred.davison@outlook.com>
2022-06-21 23:18:33 -0500

    :sparkles: add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import './reset.css';
-import styled from 'styled-components';
+import {Home} from './pages/home';
+import {Routes, Route} from 'react-router-dom';
 
-const StyledDiv = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding-bottom: 150px;
-  box-sizing: border-box;
-  font-family: 'Arial Rounded MT Bold', sans-serif;
-  h1 {
-    font-size: 2rem;
-  }
-`;
-
-export const App = () =>  {
-  return (
-    <StyledDiv>
-        <h1>React App template</h1>
-    </StyledDiv>
-  );
+export const App = () => {
+    return (
+        <Routes>
+            <Route path='*' element={<Home/>}/>
+        </Routes>
+    );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 150px;
+  box-sizing: border-box;
+  font-family: 'Arial Rounded MT Bold', sans-serif;
+
+  h1 {
+    font-size: 2rem;
+  }`;
+
+export const Home = () => {
+    return (
+        <StyledDiv>
+            <h1>React app template</h1>
+        </StyledDiv>
+    )
+};
